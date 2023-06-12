49aef2a68f29c036deb513f933b15a62d63869ca
Sergiy Kukunin <sergey.kukunin@gmail.com>
2023-06-11 21:31:06 -0700

    Implement design v1

--- src/factory.ts
+++ src/factory.ts
@@ -1,15 +1,45 @@
 interface FactoryDSL {
-  attribute: (name: string, value: () => any) => void
+  attribute: (name: string, value: () => any) => void;
 }
 
 interface DefinitionDSL {
-  factory: (name: string, factory: (dsl: FactoryDSL) => void) => void
+  factory: (name: string, factory: (dsl: FactoryDSL) => void) => void;
 }
 
 interface Factory {
-  build: (name: string, overrides?: any) => any
+  build: (name: string) => any;
+}
+
+interface FactoryDefinition {
+  attributes: { [name: string]: () => any };
 }
 
 export const define = (definition: (dsl: DefinitionDSL) => void): Factory => {
-  return {} as any
-}
+  const factories: { [name: string]: FactoryDefinition } = {};
+
+  definition({
+    factory: (name, factoryFn) => {
+      const factory: FactoryDefinition = {
+        attributes: {},
+      }
+
+      factoryFn({
+        attribute: (name, valueFn) => {
+          factory.attributes[name] = valueFn;
+        }
+      })
+
+      factories[name] = factory
+    }
+  });
+
+  return {
+    build: (name) => {
+      const factory = factories[name];
+      return Object.entries(factory.attributes).reduce((attributes, [key, value]) => ({
+        ...attributes,
+        [key]: value(),
+      }), {})
+    },
+  };
+};
